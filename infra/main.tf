dff98c731c2d793732087377d37ad1999f951d47
Yakir Perlin <yakirperlin@gmail.com>
2025-03-23 12:57:46 +0200

    Rename Lambda function resource for email parsing: Updated the resource name from 'my_lambda' to 'parsing_lambda' in the S3 bucket notification and permission configurations to improve clarity and maintainability.

--- infra/main.tf
+++ infra/main.tf
@@ -374,7 +374,7 @@ resource "aws_s3_bucket_policy" "public_access_policy" {
  }
 
 ####3 parse email lambda
-resource "aws_lambda_function" "my_lambda" {
+resource "aws_lambda_function" "parsing_lambda" {
   function_name = "email-parser-lambda-function"
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_function.lambda_handler"
@@ -505,7 +505,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.emails_bucket.bucket
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.my_lambda.arn
+    lambda_function_arn = aws_lambda_function.parsing_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
 
@@ -515,7 +515,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 resource "aws_lambda_permission" "allow_s3_to_invoke" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
+  function_name = aws_lambda_function.parsing_lambda.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.emails_bucket.arn
 }
