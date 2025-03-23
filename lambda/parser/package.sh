fb8488ac8e53cca793ccf75eb109bccb2fab3cf2
Yakir Perlin <yakirperlin@gmail.com>
2025-03-23 08:59:24 +0200

    Remove packaging script for Deno function: Deleted the package_lambda.sh script, streamlining the packaging process as it is no longer needed.

--- lambda/parser/package.sh
+++ lambda/parser/package.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# MIT License
-# Copyright (c) 2023 [Your Name or Organization]
-# See LICENSE file for details
 
 # Get the current directory name (function name)
 FUNCTION_NAME=$(basename "$PWD")
