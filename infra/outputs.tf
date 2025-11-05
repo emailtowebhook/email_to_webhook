7fb26926d634ffc29886681d3a30a629729a1fcc
Yakir Perlin <yakirperlin@gmail.com>
2025-11-05 22:33:54 +0200

    Update API Gateway output to use fixed "prod" stage instead of variable environment for consistency in deployment.

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
 # Output the API Gateway endpoint
 output "api_gateway_url" {
-  value       = "${aws_apigatewayv2_api.lambda_api.api_endpoint}/${var.environment}/v1/domain"
+  value       = "${aws_apigatewayv2_api.lambda_api.api_endpoint}/prod/v1/domain"
   description = "API Gateway endpoint URL"
 }
 
